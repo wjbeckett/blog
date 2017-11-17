639324ad9e45c930c1c1289adc2f89bed1ab3a9c
William Beckett <wjbeckett@gmail.com>
2017-11-17 15:54:26 +1000

    testing gup changes again

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,6 @@
 'use strict';
+var changed     = require('gulp-changed');
+var responsive  = require('gulp-responsive');
 
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
@@ -43,7 +45,7 @@ gulp.task('minify-images', () => {
 // LazyLoad images.
 gulp.task('images:lazyload', () => {
   gulp.src('images/lazyload/**/*.+(jpg|JPG|jpeg|JPEG|png|PNG|svg|SVG|gif|GIF|webp|WEBP|tif|TIF)', '/images/lazyload/**/*.{gif,svg}')
-  .pipe(changed(paths.imageFilesSite))
+  .pipe(changed('_site/images'))
   .pipe(responsive({
     // resize all images
     '*.*': [{
